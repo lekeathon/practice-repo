7899e40e512d7c198bf70552be30bdafd730126b
lekeathon wilson <lekeathon@gmail.com>
2026-03-02 13:25:13 -0500

    working with while loops now

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-for (let i = 0; i <= 10; i++) {
-    console.log('i am awesome');
-    for (let j = 1; j <= 5; j++) {
-        console.log(`im still awesome ${j}`);
-    }
+//for (let i = 0; i <= 10; i++) {
+// console.log('i am awesome');
+// for (let j = 1; j <= 5; j++) {
+//     console.log(`im still awesome ${j}`);
+//}
+//}
+
+//let i = 1;
+//while (i <= 10) {
+//  i++;
+//console.log(' i think im awesome');
+
+//}
+
+/*let secret = 'Flash';
+let guess = prompt('who is the fastest superhero?');
+while (guess !== secret) {
+    guess = prompt('Wrong again who is the fastest superhero?');
+
 }
+
+alert('you are correct!');
+*/
